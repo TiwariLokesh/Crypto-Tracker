a1a00b76e66d49dc0529d46db3ffc01acc0b54e7
Your Name <lokeshtiwari212@gmail.com>
2024-02-09 11:12:57 +0530

    script

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("#searchForm");
+form.addEventListener('submit',(e)=>{
+e.preventDefault();   //refreshing will be prevented
+    const ctype = form.elements.coinType.value;
+    console.log(ctype);
+})
