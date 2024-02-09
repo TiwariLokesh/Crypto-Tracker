267ac7b6f939b75b4af94c35e48a53ded676a1ca
Your Name <lokeshtiwari212@gmail.com>
2024-02-09 15:31:51 +0530

    script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("#searchForm");
-const res = document.querySelector("#result");
+const res = document.querySelector("#tableResult");
 
 form.addEventListener('submit',(e)=>{
 e.preventDefault();   //refreshing will be prevented
@@ -17,5 +17,24 @@ const fetchPrice = async(ctype)=>{
      const time = r.data.timestamp; 
      
      
-     res.innerHTML = `${price}`;
+     res.innerHTML = ` <tr>
+     <td>Property</td>
+     <td>Value</td>
+   </tr>
+   <tr>
+     <td>${base}</td>
+     <td>${price} ${target}</td>
+   </tr>
+   <tr>
+     <td>Volume</td>
+     <td>${volume}</td>
+   </tr>
+   <tr>
+     <td>Change</td>
+     <td>${change}</td>
+   </tr>
+   <tr>
+     <td>Last Update</td>
+     <td>${time}</td>
+   </tr>`;
 }
